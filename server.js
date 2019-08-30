3c75c59155fc5530381db08a279f80b15233b46a
taehunlim <dkslsdkdl@naver.com>
2019-08-30 15:23:14 +0900

    requst test

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
+
+
+
+app.use((req, res, next) => {
+    res.json({
+        message: 'It works!'
+    })
+});
+
+
+
+
 const port = 3000;
 
 const server = http.createServer(app);
