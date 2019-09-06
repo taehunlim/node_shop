cdb8aa0aedce4bcbbe7a954eab4d70bb05b63e19
taehunlim <dkslsdkdl@naver.com>
2019-09-06 20:25:40 +0900

    deleteProduct and detailProduct

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const productModel = require('../models/product');
 
-
+// total Product 불러오기
 router.get('/', (req, res) => {
 
     productModel
@@ -13,6 +13,7 @@ router.get('/', (req, res) => {
         .then(docs => {
             res.json({
                 msg: "successful Products Get",
+                count : docs.length,
                 products: docs
             });
         })
@@ -24,7 +25,7 @@ router.get('/', (req, res) => {
 
 });
 
-
+// product 등록하기
 router.post('/', (req, res) => {
 
     const product = new productModel({
@@ -50,6 +51,7 @@ router.post('/', (req, res) => {
 
 });
 
+//수정하기
 router.patch('/', (req, res) => {
     res.json({
         message : "productModefie"
@@ -57,10 +59,54 @@ router.patch('/', (req, res) => {
 });
 
 
-router.delete('/', (req, res) => {
-    res.json({
-        message : "productDelite"
-    });
+router.get('/:productId', (req, res) => {
+    const id = req.params.productId;
+
+    productModel
+        .findById(id)
+        .exec()
+        .then(result => {
+            if (!result) {
+                return res.status(404).json({
+                   msg : "no productId"
+                });
+            } else {
+                res.json({
+                    msg: "succesful detail product",
+                    productInfo: result
+                });
+            }
+        })
+        .catch(err =>{
+            res.json({
+                errInfo : err
+            });
+        });
+});
+
+
+//삭제하기
+router.delete('/:productId', (req, res) => {
+    const id = req.params.productId;
+
+    productModel
+        .remove({_id: id})
+        .exec()
+        .then(result =>{
+          res.json({
+             msg : "deletedProduct",
+             productList : result
+          });
+        })
+        .catch(err =>{
+            res.json({
+                errInfo : err
+            });
+        });
+
+
+
+
 });
 
 
