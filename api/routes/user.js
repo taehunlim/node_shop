43fe4086154e37d6ea2881c25abe4c20fabd450e
taehunlim <dkslsdkdl@naver.com>
2019-09-17 20:08:53 +0900

    sign up #1

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+//mongoose : save, find, update 등등
+
+const userModel = require('../models/user');
 
 
-router.get('/signin', (req, res) => {
-   res.json({
-    message : "userRoutes"
-   }); 
+
+
+
+router.post('/signup', (req, res) => {
+    const user = new userModel({
+        _id : new mongoose.Types.ObjectId(),
+       email : req.body.email,
+       password : req.body.password
+
+    });
+    user.save()
+        .then(user => {
+            res.status(200).json({
+                msg : "created sign up",
+                createdUser : user
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                errInfo : err
+            });
+        });
 });
-
 
 
 
