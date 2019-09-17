553e83502eb57a2efef80e1853419f9078d94a7a
taehunlim <dkslsdkdl@naver.com>
2019-09-17 20:36:07 +0900

    sign up #4 email regex
    이메일 형식 체크

--- api/models/user.js
+++ api/models/user.js
@@ -2,7 +2,12 @@
 const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     _id : mongoose.Schema.Types.ObjectId,
-    email : {type : String, required : true},
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+        match : /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+    },
     password : {type : String, required : true}
 });
 
